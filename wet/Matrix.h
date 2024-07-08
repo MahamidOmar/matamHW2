a284754486aeee3c51c36f659d8021a71e23d3e9
MahamidOmar <omarm@campus.technion.ac.il>
2024-07-08 22:11:12 +0300

    Added all the required operators for Matrix

--- wet/Matrix.h
+++ wet/Matrix.h
@@ -2,6 +2,8 @@
 #ifndef MATAMHW2_MATRIX_H
 #define MATAMHW2_MATRIX_H
 
+#include <iostream>
+
 
 class Matrix {
 private:
@@ -23,6 +25,42 @@ public:
 
     ////    operator= for matrix
     Matrix& operator=(const Matrix& matrix);
+
+    ////    operator() for reading
+    int operator()(int row, int column) const;
+
+    ////    operator() for writing
+    int& operator()(int row, int column);
+
+    ////    operator << for printing
+    friend std::ostream& operator<<(std::ostream& os, const Matrix& h);
+
+    ////    arithmetic operators for Matrix
+    Matrix operator+(const  Matrix& matrix) const;
+
+    Matrix operator-(const  Matrix& matrix) const;
+
+    Matrix operator*(const  Matrix& matrix) const;
+
+    Matrix& operator+=(const Matrix& matrix);
+
+    Matrix& operator-=(const Matrix& matrix);
+
+    Matrix& operator*=(const Matrix& matrix);
+
+    Matrix operator-()const;
+
+    friend Matrix operator*(int scalar, const Matrix& matrix);
+
+    friend Matrix operator*(const Matrix& matrix, int scalar);
+
+    Matrix& operator*=(int scalar);
+
+    ////    operators for comparing matrix
+
+    friend bool operator==(const Matrix& h1, const Matrix& h2);
+
+    friend bool operator!=(const Matrix& h1, const Matrix& h2);
 
 };
 
